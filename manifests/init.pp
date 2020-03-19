d843e0a3e4c9107a1846b99d20573fb68ff0e80b
Jarret Lavallee <jarret.lavallee@puppet.com>
2020-03-19 14:58:16 -0600

    Fixed some liniting

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,6 @@
 class git_demo::git_demo {
-  $data = lookup({'name'  => 'git_demo::participants', 'merge' => { 'strategy' => deep } })
+  $data = lookup({'name'  => 'git_demo::participants',
+                 'merge' => { 'strategy' => deep } })
 
   $data.each |$item| {
     notice("${item['name']}'s favorite food: ${item['favorite']}")
