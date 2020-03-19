d9de2014088d6373d8f7d1de038a0eae7f351db5
Chris Webster <chirs.webster@puppet.com>
2020-03-19 13:59:59 -0700

    Changed data to data participant

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 class git_demo::git_demo {
-  $data = lookup({'name'  => 'git_demo::participants', 'merge' => { 'strategy' => deep } })
+  $dataparticipant = lookup({'name'  => 'git_demo::participants', 'merge' => { 'strategy' => deep } })
 
-  $data.each |$item| {
+  $dataparticipant.each |$item| {
     notice("${item['name']}'s favorite food: ${item['favorite']}")
   }
 
