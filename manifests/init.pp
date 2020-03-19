14d2c8229d34d642e368601d5bfa71db1af832e4
Adrian <sensei.loafage@gmail.com>
2020-03-18 17:42:20 -0700

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,10 @@
+class git_demo::git_demo {
+  $data = lookup({'name'  => 'git_demo::participants', 'merge' => { 'strategy' => deep } })
+
+  $data.each |$item| {
+    notice("${item['name']}'s favorite food: ${item['favorite']}")
+  }
+
+}
+
+include git_demo::git_demo
